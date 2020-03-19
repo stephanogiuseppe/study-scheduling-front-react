6ffa68c9e535db554d376172e732747fd731c5df
stephanogiuseppe <stephano.grp@gmail.com>
2020-03-19 20:59:41 -0300

    chore: add form field validator

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
 
 import logo from '~/assets/logo.svg'
 
 const SignIn = () => {
+  const handleSubmit = (data) => {
+    console.tron.log(data)
+  }
+
   return <>
     <img src={logo} alt="Study Scheduling"/>
 
     <h2>Login</h2>
 
-    <form>
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="Password" />
+    <Form onSubmit={handleSubmit}>
+      <Input name="email" type="email" placeholder="E-mail" />
+      <Input name="password" type="password" placeholder="Password" />
 
       <button type="submit">Enter</button>
       <Link to="/register">Create account</Link>
-    </form>
+    </Form>
   </>
 }
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
 
 import logo from '~/assets/logo.svg'
 
 const SignUp = () => {
+  const handleSubmit = (data) => {
+    console.tron.log(data)
+  }
+
   return <>
     <img src={logo} alt="Study Scheduling"/>
 
     <h2>Create Account</h2>
 
-    <form>
-      <input type="text " placeholder="Name" />
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm password" />
+    <Form onSubmit={handleSubmit}>
+      <Input name="name" type="text " placeholder="Name" />
+      <Input name="email" type="email" placeholder="E-mail" />
+      <Input name="password" type="password" placeholder="Password" />
+      <Input name="confirm" type="password" placeholder="Confirm password" />
 
       <button type="submit">Create account</button>
       <Link to="/">Back to Login</Link>
-    </form>
+    </Form>
   </>
 }
 
