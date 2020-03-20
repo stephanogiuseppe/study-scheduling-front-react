ab6f3ca4556088142db2b205d84242a1cf68a3e9
stephanogiuseppe <stephano.grp@gmail.com>
2020-03-19 22:34:28 -0300

    chore: sote configuration (redux and redux-saga)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 
 import './config/ReactotronConfig'
 import Routes from './routes'
 import history from './services/history'
 import GlobalStyle from './styles/global'
+import store from './store'
 
 function App() {
-  return <Router history={history}>
-    <Routes />
-    <GlobalStyle />
-  </Router>
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
+  )
 }
 
 export default App
